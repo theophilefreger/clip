5083ec029ce8328c9f2c14ee2b3f82d47919aedd
Théophile Fréger <tfreger@me.com>
2023-12-21 18:34:43 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,41 +2,46 @@ from io import BytesIO
 from sys import stderr
 
 import typer
-import clip
 import torch
 from PIL import Image
 from torch import nn
 
+# Importations spécifiques à clip-as-service
+from clip_server.model.clip_model import CLIPModel
+from clip_server.model.tokenization import Tokenizer
+
 from sist2 import Sist2Index, serialize_float_array, print_progress
 
+# Définir le périphérique pour le calcul (GPU si disponible)
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-
 print(f"Using compute device {DEVICE}")
 
-
-def load_tag_embeddings(tag_file, model):
+# Fonction pour charger les embeddings des tags
+def load_tag_embeddings(tag_file, model, tokenizer):
     with open(tag_file) as f:
         tags = [line.strip() for line in f]
 
-    text_tokenized = clip.tokenize(tags).to(DEVICE)
+    # Utilisation du tokenizer de clip-as-service
+    tokenized = tokenizer(tags, context_length=77, truncate=True)
+    text_tokenized = tokenized['input_ids'].to(DEVICE)
+    attention_mask = tokenized['attention_mask'].to(DEVICE)
+
     with torch.no_grad():
-        tag_embeddings = model.encode_text(text_tokenized)
+        tag_embeddings = model.encode_text(text_tokenized, attention_mask)
 
     print(f"Pre-computed embeddings for {len(tags)} tags")
-
     return tag_embeddings, tags
 
-
-def main(index_file, clip_model: str = "ViT-B/32", tags_file: str = "general.txt", num_tags: int = 1, color="#dcd7ff"):
-    model, preprocess = clip.load(clip_model, device=DEVICE)
+# Fonction principale
+def main(index_file, clip_model: str = "M-CLIP/XLM-Roberta-Large-Vit-B-16Plus", tags_file: str = "general.txt", num_tags: int = 1, color="#dcd7ff"):
+    # Chargement du modèle et du tokenizer
+    model = CLIPModel(clip_model)
+    tokenizer = Tokenizer(clip_model)
     cosine_sim = nn.CosineSimilarity()
 
-    tag_embeddings, tags = load_tag_embeddings(tags_file, model)
+    tag_embeddings, tags = load_tag_embeddings(tags_file, model, tokenizer)
 
     index = Sist2Index(index_file)
-
-    # Only consider documents that were modified since the last run of this script
-    clip_version = index.get("clip_version", default=0)
 
     index.register_model(
         id=1,
@@ -47,16 +52,15 @@ def main(index_file, clip_model: str = "ViT-B/32", tags_file: str = "general.txt
         type="flat"
     )
 
+    # Définition de la condition pour les documents à traiter
     where = f"version > {clip_version} AND ((SELECT name FROM mime WHERE id=document.mime) LIKE 'image/%' OR " \
             f"(SELECT name FROM mime WHERE id=document.mime) LIKE 'video/%')"
     total = index.document_count(where)
     done = 0
 
     for doc in index.document_iter(where):
-
         try:
             if doc.parent or doc.mime.startswith("video/"):
-
                 tn = index.get_thumbnail(doc.id)
                 if not tn:
                     raise Exception("Could not find thumbnail")
@@ -64,12 +68,15 @@ def main(index_file, clip_model: str = "ViT-B/32", tags_file: str = "general.txt
                 image = Image.open(BytesIO(tn))
             else:
                 image = Image.open(doc.path)
-            image = preprocess(image).unsqueeze(0).to(DEVICE)
+            # Pas de prétraitement spécifique ici, à ajuster si nécessaire
+            image = image.convert('RGB')
         except Exception as e:
             print(f"Could not load image {doc.rel_path}: {e}", file=stderr)
             continue
 
         with torch.no_grad():
+            # Génération des embeddings pour l'image
+            # À ajuster selon les besoins spécifiques du modèle XLM-Roberta-Large-Vit-B-16Plus
             embeddings = model.encode_image(image)
 
         if num_tags > 0:
@@ -104,7 +111,6 @@ def main(index_file, clip_model: str = "ViT-B/32", tags_file: str = "general.txt
     index.commit()
 
     print("Done!")
-
 
 if __name__ == "__main__":
     typer.run(main)
