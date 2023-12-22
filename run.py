9341efb5bb9bec14e34a192100b61f20f294fc62
Théophile Fréger <tfreger@me.com>
2023-12-22 11:23:51 +0100

    Update run.py

--- run.py
+++ run.py
@@ -18,11 +18,10 @@ DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 print(f"Using compute device {DEVICE}")
 
 # Nouvelle fonction de transformation adaptée à CLIP
-def get_transform(n_px=224):
+def get_transform():
     return Compose([
-        Resize(n_px, interpolation=BICUBIC),
-        CenterCrop(n_px),
-        lambda image: image.convert('RGB'),
+        Resize(224, interpolation=Image.BICUBIC),
+        CenterCrop(224),
         ToTensor(),
         Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711))
     ])
